327d3280d569f18b8a6679c91645e5cc277356a1
AazhmeerChhapra <aazhmeerchhapra@gmail.com>
2025-02-18 11:12:11 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Medical Chatbot using LLAMA 2",
+    version="0.0.1",
+    author="Aazhmeer Chhapra",
+    author_email="aazhmeerchhapra@gmail.com",
+    packages=find_packages(),
+    install_require = []
+)
